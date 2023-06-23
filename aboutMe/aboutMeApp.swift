69654e6508f75685f1d0c9cef1280d05fff68b7c
Tea Huang <Teaghuang@gmail.com>
2023-06-23 14:58:50 -0400

    Initial Commit

---
+++ aboutMe/aboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeApp.swift
+//  aboutMe
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
